da79139a91b7b6f7cd6e9f52c6ad262f03dbbd20
SubramanyahPiktorlabs <subramanyaadiga17@gmail.com>
2020-02-26 15:02:00 +0530

    Bug Fix in app section

--- jsonLibrary/app/src/main/java/com/example/jsonlibrary/DisplayList.kt
+++ jsonLibrary/app/src/main/java/com/example/jsonlibrary/DisplayList.kt
@@ -1,9 +1,11 @@
 package com.example.jsonlibrary
 
 import android.app.DownloadManager
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
@@ -13,40 +15,49 @@ import okhttp3.*
 import org.w3c.dom.Text
 import java.io.IOException
 
-class DisplayList : AppCompatActivity() {
+public class DisplayLists {
 
-    override public fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    public fun list(c: Context, message: String) {
 
-        recycler_view_main.layoutManager = LinearLayoutManager(this)
+        Toast.makeText(c, message, Toast.LENGTH_LONG).show()
 
-        fetchJson()
-    }
-    private fun fetchJson(){
-        val url = "https://api.myjson.com/bins/18r9ww"
-
-        val request = Request.Builder().url(url).build()
-
-        val client = OkHttpClient()
-        client.newCall(request).enqueue(object: Callback {
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body?.string()
-                println(body)
-
-                val gson = GsonBuilder().create()
-                val homeFeed = gson.fromJson(body,HomeFeed::class.java)
-
-                runOnUiThread {
-                    recycler_view_main.adapter=MainAdapter(homeFeed)
-                }
-            }
-            override fun onFailure(call: Call, e: IOException) {
-                println("Failed to execute")
-            }
-        })
     }
 }
+
+//class DisplayList : AppCompatActivity() {
+//
+//    override public fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        recycler_view_main.layoutManager = LinearLayoutManager(this)
+//
+//        fetchJson()
+//    }
+//    private fun fetchJson(){
+//        val url = "https://api.myjson.com/bins/18r9ww"
+//
+//        val request = Request.Builder().url(url).build()
+//
+//        val client = OkHttpClient()
+//        client.newCall(request).enqueue(object: Callback {
+//            override fun onResponse(call: Call, response: Response) {
+//                val body = response.body?.string()
+//                println(body)
+//
+//                val gson = GsonBuilder().create()
+//                val homeFeed = gson.fromJson(body,HomeFeed::class.java)
+//
+//                runOnUiThread {
+//                    recycler_view_main.adapter=MainAdapter(homeFeed)
+//                }
+//            }
+//            override fun onFailure(call: Call, e: IOException) {
+//                println("Failed to execute")
+//            }
+//        })
+//    }
+//}
 
 //class HomeFeed(val hobbies: List<Hobby>)
 //class Hobby (val hobby: String)
